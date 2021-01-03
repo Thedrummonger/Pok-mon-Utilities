b390988f62d95ce9bec4953e675a210c1e11e6aa
Thedrummonger <drummerxtmail@gmail.com>
2021-01-03 10:20:44 -0600

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Pokemon_Utils
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NUDDisplay = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BTNPreview = new System.Windows.Forms.Button();
+            this.BTNScan = new System.Windows.Forms.Button();
+            this.LBResults = new System.Windows.Forms.ListBox();
+            this.NUDTolerance = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDTolerance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Display With Game";
+            // 
+            // NUDDisplay
+            // 
+            this.NUDDisplay.Location = new System.Drawing.Point(142, 11);
+            this.NUDDisplay.Name = "NUDDisplay";
+            this.NUDDisplay.Size = new System.Drawing.Size(38, 20);
+            this.NUDDisplay.TabIndex = 1;
+            this.NUDDisplay.ValueChanged += new System.EventHandler(this.ChangeDisplay);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your Screen Count: ";
+            // 
+            // BTNPreview
+            // 
+            this.BTNPreview.Location = new System.Drawing.Point(16, 48);
+            this.BTNPreview.Name = "BTNPreview";
+            this.BTNPreview.Size = new System.Drawing.Size(164, 23);
+            this.BTNPreview.TabIndex = 3;
+            this.BTNPreview.Text = "Preview Screen";
+            this.BTNPreview.UseVisualStyleBackColor = true;
+            this.BTNPreview.Click += new System.EventHandler(this.PreviewButtonClick);
+            // 
+            // BTNScan
+            // 
+            this.BTNScan.Location = new System.Drawing.Point(16, 77);
+            this.BTNScan.Name = "BTNScan";
+            this.BTNScan.Size = new System.Drawing.Size(164, 23);
+            this.BTNScan.TabIndex = 4;
+            this.BTNScan.Text = "Scan For Pokemon Data";
+            this.BTNScan.UseVisualStyleBackColor = true;
+            this.BTNScan.Click += new System.EventHandler(this.ScanButtonClick);
+            // 
+            // LBResults
+            // 
+            this.LBResults.FormattingEnabled = true;
+            this.LBResults.Location = new System.Drawing.Point(16, 132);
+            this.LBResults.Name = "LBResults";
+            this.LBResults.Size = new System.Drawing.Size(164, 147);
+            this.LBResults.Sorted = true;
+            this.LBResults.TabIndex = 5;
+            this.LBResults.DoubleClick += new System.EventHandler(this.ResultsListBox_DoubleClick);
+            // 
+            // NUDTolerance
+            // 
+            this.NUDTolerance.DecimalPlaces = 1;
+            this.NUDTolerance.Location = new System.Drawing.Point(122, 106);
+            this.NUDTolerance.Name = "NUDTolerance";
+            this.NUDTolerance.Size = new System.Drawing.Size(58, 20);
+            this.NUDTolerance.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Similarity Tolerance";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 288);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NUDTolerance);
+            this.Controls.Add(this.LBResults);
+            this.Controls.Add(this.BTNScan);
+            this.Controls.Add(this.BTNPreview);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NUDDisplay);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "PKM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NUDDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDTolerance)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NUDDisplay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BTNPreview;
+        private System.Windows.Forms.Button BTNScan;
+        private System.Windows.Forms.ListBox LBResults;
+        private System.Windows.Forms.NumericUpDown NUDTolerance;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using IronOcr;
+using System.Text.RegularExpressions;
+
+namespace Pokemon_Utils
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static Screen CaptureDisplay;
+        const int ENUM_CURRENT_SETTINGS = -1;
+
+        #region FormControls
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int Screencount = Screen.AllScreens.Count();
+            NUDDisplay.Value = 1;
+            NUDDisplay.Minimum = 1;
+            NUDDisplay.Maximum = Screencount;
+
+            label2.Text = "Current Display: " + Screen.AllScreens[0].DeviceName;
+
+            NUDTolerance.Value = 0.8M;
+            NUDTolerance.Minimum = 0.1M;
+            NUDTolerance.Maximum = 1;
+            NUDTolerance.Increment = 0.1M;
+            NUDTolerance.DecimalPlaces = 1;
+        }
+
+        private void PreviewButtonClick(object sender, EventArgs e)
+        {
+            var image = Screenshot(Screen.AllScreens[(int)NUDDisplay.Value - 1]);
+
+            Form ImageDisplay = new Form();
+            ImageDisplay.Width = 960;
+            ImageDisplay.Height = 540;
+            ImageDisplay.BackgroundImage = image;
+            ImageDisplay.BackgroundImageLayout = ImageLayout.Stretch;
+            ImageDisplay.Show();
+
+        }
+
+        private void ChangeDisplay(object sender, EventArgs e)
+        {
+            label2.Text = "Current Display: " + Screen.AllScreens[(int)NUDDisplay.Value - 1].DeviceName;
+        }
+
+        private void ScanButtonClick(object sender, EventArgs e)
+        {
+            LBResults.Items.Clear();
+            Regex trimmer = new Regex(@"\s\s+");
+
+            var image = Screenshot(Screen.AllScreens[(int)NUDDisplay.Value - 1]);
+            var Result = new IronTesseract().Read(image);
+
+            var s = trimmer.Replace(Result.Text, " ");
+            s = s.Replace(".", "");
+
+            string[] words = s.Split(' ');
+
+            foreach (var WordFromScreen in words.Where(x => !string.IsNullOrWhiteSpace(x)))
+            {
+                foreach(var PokemonName in PKMN.Names)
+                {
+                    double Simularity = CalculateSimilarity(WordFromScreen, PokemonName);
+                    if (Simularity >= (double)NUDTolerance.Value && !LBResults.Items.Contains(PokemonName))
+                    {
+                        LBResults.Items.Add(PokemonName);
+                    }
+                }
+            }
+
+        }
+
+        private void ResultsListBox_DoubleClick(object sender, EventArgs e)
+        {
+            var pokemon = LBResults.SelectedItem.ToString();
+            System.Diagnostics.Process.Start("https://bulbapedia.bulbagarden.net/wiki/" + pokemon + "#Type_effectiveness");
+        }
+
+        #endregion FormControls
+
+        //====================================================================================================================
+
+        #region Dekstop Screen Capture
+
+        public static Bitmap Screenshot(Screen screen)
+        {
+            DEVMODE dm = new DEVMODE();
+            dm.dmSize = (short)Marshal.SizeOf(typeof(DEVMODE));
+            EnumDisplaySettings(screen.DeviceName, ENUM_CURRENT_SETTINGS, ref dm);
+
+            Bitmap screenshot;
+
+            using (Bitmap bmp = new Bitmap(dm.dmPelsWidth, dm.dmPelsHeight))
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(dm.dmPositionX, dm.dmPositionY, 0, 0, bmp.Size);
+                screenshot = new Bitmap(bmp, 1024, 768);
+                //bmp.Save(screen.DeviceName.Split('\\').Last() + ".png");
+            }
+
+
+            return screenshot;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumDisplaySettings(string lpszDeviceName, int iModeNum, ref DEVMODE lpDevMode);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEVMODE
+        {
+            private const int CCHDEVICENAME = 0x20;
+            private const int CCHFORMNAME = 0x20;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public ScreenOrientation dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+
+        #endregion Dekstop Screen Capture
+
+        //====================================================================================================================
+
+        #region Text Similarity Testing
+
+        public static double CalculateSimilarity(string source, string target)
+        {
+            if ((source == null) || (target == null)) return 0.0;
+            if ((source.Length == 0) || (target.Length == 0)) return 0.0;
+            if (source == target) return 1.0;
+
+            int stepsToSame = LevenshteinDistance(source, target);
+            return (1.0 - ((double)stepsToSame / (double)Math.Max(source.Length, target.Length)));
+        }
+
+        public static int LevenshteinDistance(string source, string target)
+        {
+            // degenerate cases
+            if (source == target) return 0;
+            if (source.Length == 0) return target.Length;
+            if (target.Length == 0) return source.Length;
+
+            // create two work vectors of integer distances
+            int[] v0 = new int[target.Length + 1];
+            int[] v1 = new int[target.Length + 1];
+
+            // initialize v0 (the previous row of distances)
+            // this row is A[0][i]: edit distance for an empty s
+            // the distance is just the number of characters to delete from t
+            for (int i = 0; i < v0.Length; i++)
+                v0[i] = i;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                // calculate v1 (current row distances) from the previous row v0
+
+                // first element of v1 is A[i+1][0]
+                //   edit distance is delete (i+1) chars from s to match empty t
+                v1[0] = i + 1;
+
+                // use formula to fill in the rest of the row
+                for (int j = 0; j < target.Length; j++)
+                {
+                    var cost = (source[i] == target[j]) ? 0 : 1;
+                    v1[j + 1] = Math.Min(v1[j] + 1, Math.Min(v0[j + 1] + 1, v0[j] + cost));
+                }
+
+                // copy v1 (current row) to v0 (previous row) for next iteration
+                for (int j = 0; j < v0.Length; j++)
+                    v0[j] = v1[j];
+            }
+
+            return v1[target.Length];
+        }
+
+        #endregion Text Similarity Testing
+    }
+}
